237806805d7e58e6fccc07394589dfdaf0d583a0
Youssef Shaaban <youssefshaaban112000@gmail.com>
2023-01-09 15:10:49 +0200

    frontend: register form

--- frontend/src/components/authComponents/RegisterComponent.tsx
+++ frontend/src/components/authComponents/RegisterComponent.tsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
 
 const RegisterComponent = () => {
-  return (
-    <div className='register-component'>
-        <h2>RegisterComponent</h2>
-    </div>
-  )
-}
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm();
 
-export default RegisterComponent
+  const onSubmit = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="auth-component register">
+      <h2>Register Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="input-container">
+          <div className="input-field flex-column">
+            <label htmlFor="username">User name</label>
+            <input
+              type="text"
+              placeholder="Ex. John Doe"
+              {...register("username", {
+                required: true,
+                minLength: 3,
+                maxLength: 30,
+              })}
+            />
+          </div>
+          {errors.username?.type ? (
+            <span className="error">
+              User name is required and must contain in range from 3 to 30
+              letters.
+            </span>
+          ) : null}
+        </div>
+        <div className="input-container">
+          <div className="input-field flex-column">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              placeholder="Ex. john@domain.com"
+              {...register("email", { required: true })}
+            />
+          </div>
+          {errors.email?.type ? (
+            <span className="error">Email is required and must be valid.</span>
+          ) : null}
+        </div>
+        <div className="input-container">
+          <div className="input-field flex-column">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Ex. youraccountpassword"
+              {...register("password", { required: true })}
+            />
+          </div>
+          {errors.password?.type ? (
+            <span className="error">Password is required.</span>
+          ) : null}
+        </div>
+        <button
+          type="submit"
+          disabled={
+            (errors.username || errors.email || errors.password)?.type
+              ? true
+              : false
+          }
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterComponent;
